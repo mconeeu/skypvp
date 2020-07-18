5f8d904433214804ba9e4b08db6322e4663f5dd5
Dominik Lippl <dominik.lippl@hotmail.de>
2020-07-18 17:18:10 +0200

    Implemented latest BCS and GameSystem.

--- src/main/java/eu/mcone/skypvp/util/SidebarObjective.java
+++ src/main/java/eu/mcone/skypvp/util/SidebarObjective.java
@@ -9,6 +9,7 @@ import eu.mcone.coresystem.api.bukkit.CoreSystem;
 import eu.mcone.coresystem.api.bukkit.gamemode.Gamemode;
 import eu.mcone.coresystem.api.bukkit.player.CorePlayer;
 import eu.mcone.coresystem.api.bukkit.scoreboard.CoreSidebarObjective;
+import eu.mcone.coresystem.api.bukkit.scoreboard.CoreSidebarObjectiveEntry;
 
 public class SidebarObjective extends CoreSidebarObjective {
 
@@ -17,33 +18,32 @@ public class SidebarObjective extends CoreSidebarObjective {
     }
 
     @Override
-    public void onRegister(CorePlayer player) {
-        setDisplayName(CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.1"));
+    protected void onRegister(CorePlayer corePlayer, CoreSidebarObjectiveEntry entry) {
+        entry.setTitle(CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.1"));
 
-        setScore(13, "");
-        setScore(12, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.2"));
-        setScore(11, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.3") + player.getStats(Gamemode.SKYPVP).getKill());
-        setScore(10, "");
-        setScore(9, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.4"));
-        setScore(8, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.5") + player.getStats(Gamemode.SKYPVP).getDeath());
-        setScore(7, "");
-        setScore(6, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.6"));
-        setScore(5, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.7")+ player.getCoins());
-        setScore(4, "");
-        setScore(3, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.8"));
-        setScore(2, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.9"));
-        setScore(1, "");
-        setScore(0, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.10"));
+        entry.setScore(13, "");
+        entry.setScore(12, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.2"));
+        entry.setScore(11, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.3") + player.getStats(Gamemode.SKYPVP).getKill());
+        entry.setScore(10, "");
+        entry.setScore(9, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.4"));
+        entry.setScore(8, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.5") + player.getStats(Gamemode.SKYPVP).getDeath());
+        entry.setScore(7, "");
+        entry.setScore(6, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.6"));
+        entry.setScore(5, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.7") + player.getCoins());
+        entry.setScore(4, "");
+        entry.setScore(3, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.8"));
+        entry.setScore(2, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.9"));
+        entry.setScore(1, "");
+        entry.setScore(0, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.10"));
     }
 
     @Override
-    public void onReload(CorePlayer player) {
-        setDisplayName(CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.1"));
+    protected void onReload(CorePlayer corePlayer, CoreSidebarObjectiveEntry entry) {
+        entry.setTitle(CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.1"));
 
-        setScore(11, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.3") + player.getStats(Gamemode.SKYPVP).getKill());
-        setScore(8, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.5") + player.getStats(Gamemode.SKYPVP).getDeath());
-        setScore(5, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.7") + player.getCoins());
-        setScore(2, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.9"));
+        entry.setScore(11, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.3") + player.getStats(Gamemode.SKYPVP).getKill());
+        entry.setScore(8, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.5") + player.getStats(Gamemode.SKYPVP).getDeath());
+        entry.setScore(5, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.7") + player.getCoins());
+        entry.setScore(2, CoreSystem.getInstance().getTranslationManager().get("skypvp.scoreboard.9"));
     }
-
 }
